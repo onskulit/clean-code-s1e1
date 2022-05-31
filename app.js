ddc55d89f6020b2b3f2807ad9ca405cb9a85ef97
onskulit <dislaty@mail.com>
2022-05-31 10:55:29 +0300

    refactor: update checkbox input class name according to BEM according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.classList.add("list-item__checkbox");
     editInput.type="text";
     editInput.className="task";
     editInput.classList.add("text-input");
